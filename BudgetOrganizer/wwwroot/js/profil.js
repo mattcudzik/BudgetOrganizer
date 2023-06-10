be2fac614f46122f70cea268a6cdd37fa285f682
Ompio <72317376+Ompio@users.noreply.github.com>
2023-06-10 21:31:38 +0200

    fixes

--- BudgetOrganizer/wwwroot/js/profil.js
+++ BudgetOrganizer/wwwroot/js/profil.js
@@ -27,7 +27,8 @@ window.addEventListener("load", () => {
             .then((json) => {
                 console.log(json);
                 const przychodyBody = document.getElementById("przychody_center")
-                for (let i = 0; i < json.length; i++) {
+                let incomeValue = 0;
+                for (let i = 0; (i < 4 && i < json.length)  ; i++) {
                     let obj = json[i];
                     console.log(obj);
                     var newElement = document.createElement("div");
@@ -40,14 +41,16 @@ window.addEventListener("load", () => {
 
                     var allDiv = document.createElement("div");
                     allDiv.setAttribute("class", "all");
-                    allDiv.innerHTML = '<h3>' + date.toDateString() + '</h3>' + '<p>' + obj.amount + 'dupoaPLN' + '</p>'
-
+                    allDiv.innerHTML = '<h3>' + date.toDateString() + '</h3>' + '<p>' + obj.amount + 'PLN' + '</p>'
                     newElement.appendChild(colorDiv);
                     newElement.appendChild(allDiv);
 
                     // newElement.setAttribute("style", "background-color:" + obj.color + ';');
                     przychodyBody.appendChild(newElement);
+                    incomeValue += obj.amount;
                 }
+                const podsumowanie_p = document.getElementById("podsumowanie_p")
+                podsumowanie_p.innerHTML = '<p>' + incomeValue + '</p>'
                 
             }).catch((error) => {
             console.error(error);
@@ -80,7 +83,8 @@ window.addEventListener("load", () => {
             .then((json) => {
                 console.log(json);
                 const przychodyBody = document.getElementById("wydatki_center")
-                for (let i = 0; i < json.length; i++) {
+                let outcomeValue = 0;
+                for (let i = 0; i < json.length && i < 4; i++) {
                     let obj = json[i];
                     console.log(obj);
                     var newElement = document.createElement("div");
@@ -98,9 +102,11 @@ window.addEventListener("load", () => {
                     newElement.appendChild(colorDiv);
                     newElement.appendChild(allDiv);
 
-                    // newElement.setAttribute("style", "background-color:" + obj.color + ';');
                     przychodyBody.appendChild(newElement);
+                    outcomeValue += obj.amount;
                 }
+                const podsumowanie_w = document.getElementById("podsumowanie_w")
+                podsumowanie_w.innerHTML = '<p>' + outcomeValue + '</p>'
 
             }).catch((error) => {
             console.error(error);
@@ -133,10 +139,8 @@ window.addEventListener("load", () => {
                 console.log(json);
                 const finanse = document.getElementById("finanse")
                 finanse.innerHTML = '<p>' + json.budget + '</p>'
-                const podsumowanie_p = document.getElementById("podsumowanie_p")
-                podsumowanie_p.innerHTML = '<p>' + json.budget + '</p>'
-                const podsumowanie_w = document.getElementById("podsumowanie_w")
-                podsumowanie_w.innerHTML = '<p>' + json.budget + '</p>'
+                
+                
                 
             }).catch((error) => {
             console.error(error);
@@ -144,32 +148,63 @@ window.addEventListener("load", () => {
     }
 
 
-    function sendData() {
+    // function sendData() {
+    //
+    //     const FD = new FormData(form);
+    //     var bodyPost = '{"categoryId": "' + FD.get("category")
+    //         + '","amount": "' + FD.get("value")
+    //         + '","dateTime": "' + FD.get("start-date")
+    //         + '"}';
+    //
+    //     // console.log(bodyPost);
+    //
+    //     const auth = "Bearer " + localStorage.getItem("token");
+    //     // Prepare request
+    //
+    //     const request = new Request("https://localhost:7057/api/Operations/me", {
+    //         method: "POST",
+    //         headers: {
+    //             'Accept': 'application/json',
+    //             'Content-Type': 'application/json',
+    //             'Authorization': auth
+    //         },
+    //         body: bodyPost
+    //     });
+    //
+    //     // Send request
+    //
+    //     fetch(request)
+    //         .then((response) => {
+    //             if (!response.ok) {
+    //
+    //                 return response.text().then((text) => {
+    //                     const error = response.status + ' ' + text;
+    //                     throw new Error(error)
+    //                 })
+    //             }
+    //             return response.json()
+    //         })
+    //         .then((json) => {
+    //             console.log(json);
+    //         }).catch((error) => {
+    //         console.error(error);
+    //     });
+    //
+    // }
 
-        const FD = new FormData(form);
-        var bodyPost = '{"categoryId": "' + FD.get("category")
-            + '","amount": "' + FD.get("value")
-            + '","dateTime": "' + FD.get("start-date")
-            + '"}';
-
-        // console.log(bodyPost);
-
+    function getPositiveFlourData() {
+        // Preparing operationsRequest to get my data
         const auth = "Bearer " + localStorage.getItem("token");
-        // Prepare request
-
-        const request = new Request("https://localhost:7057/api/Operations/me", {
-            method: "POST",
+        const operationsRequest = new Request("https://localhost:7057/api/Operations/me/category-report?positive=true", {
+            method: "GET",
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json',
                 'Authorization': auth
-            },
-            body: bodyPost
+            }
         });
 
-        // Send request
-
-        fetch(request)
+        fetch(operationsRequest)
             .then((response) => {
                 if (!response.ok) {
 
@@ -181,18 +216,121 @@ window.addEventListener("load", () => {
                 return response.json()
             })
             .then((json) => {
+                console.log("duoa");
                 console.log(json);
+                let labels = [];
+                let data = [];
+                let backgroundColor = [];
+                const water = document.getElementById("positiveYeast")
+                for (let i = 0; i<json.length;i++){
+                    let obj = json[i];
+                    labels.push(obj.category.name);
+                    data.push(obj.sum);
+                    backgroundColor.push(obj.category.color)
+                }
+
+                new Chart(water, {
+                    type: 'doughnut',
+                    data: {
+                        labels: labels,
+                        datasets: [{
+                            label: 'wpływ',
+                            data: data,
+                            backgroundColor: backgroundColor,
+                            borderWidth: 0.5
+                        }]
+                    },
+                    options: {
+                        plugins: {
+                            title: {
+                                display: true,
+                                text: 'Wpływy'
+                            },
+                            legend: {
+                                display: false,
+                            }
+                        }
+                    }
+                });
+
             }).catch((error) => {
             console.error(error);
         });
-
     }
+    function getNegativeFlourData() {
+        // Preparing operationsRequest to get my data
+        const auth = "Bearer " + localStorage.getItem("token");
+        const operationsRequest = new Request("https://localhost:7057/api/Operations/me/category-report?positive=false", {
+            method: "GET",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': auth
+            }
+        });
 
+        fetch(operationsRequest)
+            .then((response) => {
+                if (!response.ok) {
+
+                    return response.text().then((text) => {
+                        const error = response.status + ' ' + text;
+                        throw new Error(error)
+                    })
+                }
+                return response.json()
+            })
+            .then((json) => {
+                console.log("duoa");
+                console.log(json);
+                let labels = [];
+                let data = [];
+                let backgroundColor = [];
+                const water = document.getElementById("negativeYeast")
+                for (let i = 0; i<json.length;i++){
+                    let obj = json[i];
+                    labels.push(obj.category.name);
+                    data.push(obj.sum);
+                    backgroundColor.push(obj.category.color)
+                }
+
+                new Chart(water, {
+                    type: 'doughnut',
+                    data: {
+                        labels: labels,
+                        datasets: [{
+                            label: 'wypływ',
+                            data: data,
+                            backgroundColor: backgroundColor,
+                            borderWidth: 0.5
+                        }]
+                    },
+                    options: {
+                        plugins: {
+                            title: {
+                                display: true,
+                                text: 'Wypływy'
+                            },
+                            legend: {
+                                display: false,
+                            }
+                        }
+                    }
+                });
+
+            }).catch((error) => {
+            console.error(error);
+        });
+    }
+    
 
     const form = document.getElementById("login-form");
+    
     getIncomeOperationsData();
     getOutcomeOperationsData();
     getMyAccountData();
+    getPositiveFlourData()
+    getNegativeFlourData()
 
 });
 
